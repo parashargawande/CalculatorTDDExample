3b57e802afb9ddacf3083b8473f4ba34eb1867dd
Gawande <2022894@cognizant.com>
2022-05-16 14:32:08 +0530

    adding code to satisfy new line seperator and throw error for negative nos

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,15 @@
 function add(numbers ="") {
-    let numbersArray = numbers.split(/[,]+/)
+    let numbersArray = numbers.split(/[,\n]+/)
+    let negativeNos = []
     let sum = 0;
     numbersArray.forEach(no => {
         no = parseInt(no | 0)
         sum += no
+
+        if (no < 0) negativeNos.push(no)
     });
+    
+    if(negativeNos.length > 0) throw new Error("negatives not allowed "+ negativeNos.join(" "))
     return sum;
 }
 
--- src/calculator.spec.js
+++ src/calculator.spec.js
@@ -5,32 +5,32 @@ const assert = require('assert')
 
 // add function should exists and returns a number
 describe('Calculator module', () => {
-    it('should export add function',()=>{
+    it('should export add function', () => {
         expect(Calculator.add).to.be.a('function')
     })
 
-    describe('add function',()=>{
-        it('should return number',()=>{
+    describe('add function', () => {
+        it('should return number', () => {
             expect(Calculator.add()).to.be.a('number')
         })
-    })
 
-    //empty string return 0
-    it('should return 0 for empty string',()=>{
-        assert.equal(Calculator.add(""),0)
-    })
+        //empty string return 0
+        it('should return 0 for empty string', () => {
+            assert.equal(Calculator.add(""), 0)
+        })
 
-    //two number returns addition
-    it('should add two numbers',()=>{
-        assert.equal(Calculator.add("1,2"),3)
-    })
+        //two number returns addition
+        it('should add two numbers', () => {
+            assert.equal(Calculator.add("1,2"), 3)
+        })
 
-    it('should handle new line as seperator and more than 2 nos',()=>{
-        expect(Calculator.add("1,2\n3")).to.be.equal(6)
-    })
+        it('should handle new line as seperator and more than 2 nos', () => {
+            expect(Calculator.add("1,2\n3")).to.be.equal(6)
+        })
 
-    //negative number returns error and show all of them in error
-    it('should throw error when negative nos are given',()=>{
-        expect(Calculator.add("1,2\n3\n-2")).to.throw(new Error())
+        //negative number returns error and show all of them in error
+        it('should throw error when negative nos are given', () => {
+            expect(() => Calculator.add("1,2\n3\n-2,-4,-9")).to.throw("negatives not allowed -2 -4 -9")
+        })
     })
 })
